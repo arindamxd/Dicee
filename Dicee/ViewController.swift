9a8a43e9f53d4c19c923d9aed7ff7610a1d4ece4
Arindam Karmkar <arindamxd@gmail.com>
2021-03-17 00:16:14 +0530

    dice image alpha modified

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -15,13 +15,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        diceImageView1.alpha = 0.2
+        diceImageView2.alpha = 0.2
     }
 
     @IBAction func rollButtonPressed(_ sender: Any) {
         let imageList = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
         
-        diceImageView1.image = imageList[Int.random(in: 0...5)]
-        diceImageView2.image = imageList.randomElement()
+        diceImageView1.image = imageList.shuffled()[Int.random(in: 0...5)]
+        diceImageView2.image = imageList.shuffled().randomElement()
+        
+        diceImageView1.alpha = 1.0
+        diceImageView2.alpha = 1.0
     }
 
 }
